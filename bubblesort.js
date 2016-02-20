d122c2f3fa8fbc66a1550d13677274b44bebdbca
Brian Kildow <bkildow@gmail.com>
2016-02-19 19:53:17 -0500

    Fixed for older versions of jquery

--- bubblesort.js
+++ bubblesort.js
@@ -13,8 +13,8 @@
 
       $('#edit-play').click(function () {
         $('.messages').hide();
-        $('#edit-step').prop("disabled", true);
-        $('#edit-play').prop("disabled", true);
+        $('#edit-step').attr('disabled', true);
+        $('#edit-play').attr('disabled', true);
         drawChart();
         startLoop();
         return false;
@@ -41,8 +41,8 @@
 
         if (sorted == 1) {
           $("#main-content").after('<div class="messages messages--status">Sorted!</div>');
-          $('#edit-step').prop("disabled", true);
-          $('#edit-play').prop("disabled", true);
+          $('#edit-step').attr('disabled', true);
+          $('#edit-play').attr('disabled', true);
           if (typeof interval !== 'undefined') {
             clearInterval(interval);
           }
