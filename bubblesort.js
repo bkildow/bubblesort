b9c1fcf2da1dbaad214a0199e2945c156f74bf27
Brian Kildow <bkildow@gmail.com>
2016-02-19 19:03:03 -0500

    Added play feature

--- bubblesort.js
+++ bubblesort.js
@@ -1,27 +1,51 @@
 (function ($) {
   Drupal.behaviors.bubblesort = {
     attach: function (context, settings) {
-      $('#edit-step').click(function () {
-        drawChart();
-        return false;
-      });
-
       google.charts.load('current', {packages: ['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
 
-      function drawChart() {
-        var ajax = $.ajax({
+      $('#edit-step').click(function () {
+        $('.messages').hide();
+        drawChart();
+        return false;
+      });
+
+      $('#edit-play').click(function () {
+        $('.messages').hide();
+        $('#edit-step').prop("disabled", true);
+        $('#edit-play').prop("disabled", true);
+        drawChart();
+        startLoop();
+        return false;
+      });
+
+      function startLoop() {
+        interval = window.setInterval(function () {
+          drawChart();
+        }, 200);
+      }
+
+      function bubblesortAjaxRequest() {
+        return $.ajax({
           url: "/bubblesort_step_data",
           dataType: "json",
           async: false
         });
+      }
 
+
+      function drawChart() {
+        var ajax = bubblesortAjaxRequest()
         var sorted = ajax.getResponseHeader('X-sorted');
 
         if (sorted == 1) {
           $("#main-content").after('<div class="messages messages--status">Sorted!</div>');
           $('#edit-step').prop("disabled", true);
+          $('#edit-play').prop("disabled", true);
+          if (typeof interval !== 'undefined') {
+            clearInterval(interval);
+          }
         }
 
         // Create our data table out of JSON data loaded from server.
@@ -36,6 +60,7 @@
         var chart = new google.visualization.BarChart(document.getElementById('chart'));
         chart.draw(data, options);
 
+        return sorted;
       }
     }
   };
